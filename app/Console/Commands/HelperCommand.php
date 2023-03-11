1c82546df25f40a0ce3356f32955af29f6aa9fbf
zhangfei <1538193053@qq.com>
2023-03-11 08:57:21 +0800

    orm

--- app/Console/Commands/HelperCommand.php
+++ app/Console/Commands/HelperCommand.php
@@ -2,10 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Post;
 use App\Models\User;
 use App\Services\PostService;
-use App\Services\UserService;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 
@@ -29,119 +27,74 @@ class HelperCommand extends Command
      * Execute the console command.
      *
      * @return int
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
-     * @throws \Psr\SimpleCache\InvalidArgumentException
      */
     public function handle()
     {
-
-        /** 调试相关 */
         // dd
-        // dump('hello',['hello','world'=>'zh']);
-        // dd('hello',['hello','world'=>'zh']);
+//        User::first()->dump();
+//        User::first()->dd();
 
+        // collect
+//        $array = [
+//            ['id'=>1,'name'=>'white'],
+//            ['id'=>2,'name'=>'red'],
+//        ];
 
+//        collect($array)->dump();
+//        collect($array)->pluck('id')->dump();
+//        collect($array)->each(function ($item){
+//            dump($item);
+//            return $item;
+//        });
 
-        // 辅助判断
-        $blank = null;
-        $params = null;
-        $id = '2';
-        $name = 'John';
-//        $this->info( 'blank:'.blank($blank));
-//        $this->info( 'filled:'.filled($blank));
+        // value
+//        $callable = function (){
+//          $param = func_get_args();
+//          dump($param);
+//        };
+//
+//        value($callable,1,2,3);
 
         // app
-//        $redisKey = 'app_redis_key';
-//        app('log')->info('app info', ['hello', 'world' => 'zh']);
+//        app(PostService::class)->find();
+//        app(PostService::class,['id'=>23])->find();
+//        app(PostService::class,['id2'=>23])->fid();
+//        $key = 'app_cache';
+//        app('cache')->set($key, 1);
+//        dump(app('cache')->get($key));
+
+        // dispatch
+//        dispatch(new Demo)->onQueue()->onConnection()->delay();
+
+        // now
+//        $now = now();
+//        dump($now->toDateTimeString());
+//        dump($now->addHour()->toDateTimeString());
+//        dump($now->toDateTimeString());
+
+
+        // blank
+//        $blank = '';
+//        $empty = null;
+//        $emptyArr = [];
+//        $emptyCollection = collect([]);
+//        dump(blank($blank));
+//        dump(blank($empty));
+//        dump(blank($emptyArr));
+//        dump(blank($emptyCollection));
+
+        // optional
+//        $user = null;
+//        dump(optional($user)->id);
+//        dump(optional($user)['id']);
+//        dump(optional($user,function (&$user){
 //
-//        app('redis')->setnx($redisKey, 1);
-//        dump(app('redis')->get($redisKey));
-//
-//        $appCacheKey = 'app_cache_key';
-//        app('cache')->set($appCacheKey, 1);
-//        dump(app('cache')->get($appCacheKey));
-//
-//        app(UserService::class)->find(1);
-//
-//        app(PostService::class, ['id' => 3])->find();
+//            $user['id']=2;
+//        })['id']);
+//        optional(User::query()->where('id',99999)->first())->name;
+//        dump($user?->name);
 
 
-        // log
-//        info('info', ['hello', 'world' => 'zh']);
-//        logger('logger', ['hello', 'world' => 'zh']);
-//        logger()->error('You are not allowed here.');
-
-        with(new UserService())->find(1);
-        with(new PostService(4))->find();
-
-        dump(with($params, function ($params) {
-            if ($params == null) {
-                return 'with hello';
-            }
-
-            return $params;
-        }));
-
-        $callable = function ($name) {
-            echo $name . PHP_EOL;
-        };
-        with($name, $callable);
-
-        dump(value(function () {
-            [$id, $name] = func_get_args();
-
-            return $id . $name;
-        }, $id, $name));
-
-        dump(value(function () use ($id, $name) {
-            return $id . $name;
-        }, $id, $name));
-
-        dump(value(function ($id, $name) {
-            return $name . $id;
-        }, $id, $name));
-
-        $user = new User(['id' => 999, 'email' => 'hello@qq.com']);
-
-        dump($user->name);
-        dump(optional($user)->name);
-
-        dump(optional($user, function ($user) {
-            return $user->name;
-        }));
-
-        dump(tap(User::first(), function (User $user) {
-            dump($user->email);
-            $user->update(['email' => $user->email . now()->toDateTimeString()]);
-        })->email);
-
-        dump(tap(User::first())->update(['email' => now()->toDateTimeString()])->email);
-
-        dump(transform('call', $callable));
-
-        /** object_get */
-        $this->info(isset($user['name']));
-        $this->info(object_get($user, 'name2.post.name', 'zhangsan'));
-        $this->info($user->name2->post ?? 'zhangsan');
-
-        /** 对象操作相关 */
-
-        /** 集合相关 */
-
-        dump(Post::with('image')->limit(10)->get()->toArray());
-        dump(Post::with('image')->limit(10)->get()->pluck('image')->toArray());
-        dump(Post::with('image')->limit(10)->get()->pluck('image')->collapse()->toArray());
-
-        /* */
-//        retry(10, function () {
-//            $corpNo = random_int(100000000, 999999999);
-//            if (CorpAuth::query()->where('corp_no', $corpNo)->first()) {
-//                throw new \Exception('corp_no make failed');
-//            }
-//
-//            return $corpNo;
-//        }, 100);
         return CommandAlias::SUCCESS;
     }
 }
